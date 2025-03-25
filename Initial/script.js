039e58501393f58782a42c08dbbf2f75cd3591d9
qkrdudwls <jacob717@ajou.ac.kr>
2025-03-25 18:49:07 +0900

    script.js 작성 및 코드 업로드

--- Initial/script.js
+++ Initial/script.js
@@ -1,13 +1,40 @@
-const canvas = document.getElementById("glCanvas");
-const gl = canvas.getContext("webgl");
+"use strict";
 
-if (!gl) {
-    alert('WebGL is not available.');
+let gl;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "glCanvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL is not available" ); }
+
+    var vertices = [
+        vec2(0.0, 0.0),
+        vec2(0.5, 0.5),
+        vec2(-0.5,0.5),
+        vec2(0.0, 0.0),
+        vec2(0.0, -0.5)
+    ];
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1., 1., 1.0, 1.0 );
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.LINES, 0, 6 );
 }
-
-var vertices_Y = [
-]
-
-var vertices_J = [
-
-]
