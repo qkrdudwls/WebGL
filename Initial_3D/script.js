cc4a265220258da9aed04f2d8a599eea84022b64
qkrdudwls <jacob717@ajou.ac.kr>
2025-04-14 12:50:02 +0900

    폴더 이름 변경 및 파일 복사

---
+++ Initial_3D/script.js
@@ -0,0 +1,166 @@
+"use strict";
+
+let gl;
+let delay = 10;
+let theta = 0.0;
+let thetaLoc;
+let direction = true;
+let start = false;
+
+window.onload = function init()
+{
+    let canvas = document.getElementById( "glCanvas" );
+
+    document.getElementById( "Control" ).onclick = function () {
+        start = !start;
+        if (start) {
+            this.value = "Stop";
+            render();
+        } else {
+            this.value = "Start";
+        }
+    }
+
+    document.getElementById( "Direction" ).onclick = function () {
+        if (direction) {
+            this.value = "To the Left";
+            direction = false;
+        } else {
+            this.value = "To the Right";
+            direction = true;
+        }
+    }
+
+    document.getElementById( "Speed" ).onclick = function () {
+        delay = parseInt(this.value);
+    }
+
+    window.onkeydown = function ( event ) {
+        let key = String.fromCharCode( event.keyCode );
+        switch(key) {
+            case 'A':
+                direction = true;
+                document.getElementById( "Direction" ).value = "To the Right";
+                break;
+            case 'D':
+                direction = false;
+                document.getElementById( "Direction" ).value = "To the Left";
+                break;
+        }
+    }
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL is not available" ); }
+
+    var vertices = [
+        // Y
+        vec2(-0.5, 0.2),
+        vec2(-0.4, 0.0),
+        vec2(-0.4, 0.2),
+        vec2(-0.3, 0.0),
+        vec2(-0.2, 0.2),
+        vec2(-0.3, 0.2),
+        vec2(-0.4, 0.0),
+        vec2(-0.3, 0.0),
+        vec2(-0.4, -0.2),
+        vec2(-0.3, -0.2),
+
+        // J
+        vec2(-0.1, 0.2),
+        vec2(-0.1, 0.1),
+        vec2(0.2, 0.2),
+        vec2(0.2, 0.1),
+        vec2(0.1, 0.1),
+        vec2(0.2, -0.1),
+        vec2(0.1, -0.1),
+        vec2(0.1, -0.2),
+        vec2(0.0, -0.1),
+        vec2(0.0, -0.2),
+        vec2(-0.1, -0.1),
+        vec2(-0.1, 0.0),
+        vec2(0.0, -0.1),
+
+        // P
+        vec2(0.3, 0.2),
+        vec2(0.3, -0.2),
+        vec2(0.4, 0.2),
+        vec2(0.4, -0.2),
+        vec2(0.4, -0.1),
+        vec2(0.4, 0.0),
+        vec2(0.5, -0.1),
+        vec2(0.5, 0.0),
+        vec2(0.6, 0.0),
+        vec2(0.5, 0.1),
+        vec2(0.6, 0.1),
+        vec2(0.5, 0.2),
+        vec2(0.4, 0.2),
+        vec2(0.5, 0.1),
+        vec2(0.4, 0.1)
+    ];
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1., 1., 1.0, 1.0 );
+
+    let program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    let bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    let vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    thetaLoc = gl.getUniformLocation( program, "theta" );
+
+    render();
+};
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.uniform1f( thetaLoc, theta );
+
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.drawArrays( gl.TRIANGLES, 1, 3 );
+    gl.drawArrays( gl.LINES, 3, 2 );
+    gl.drawArrays( gl.LINES, 4, 2 );
+    gl.drawArrays( gl.POINTS, 3, 1 );
+    gl.drawArrays( gl.POINTS, 4, 1 );
+    gl.drawArrays( gl.POINTS, 5, 1 );
+    gl.drawArrays( gl.TRIANGLES, 5, 3 );
+    gl.drawArrays( gl.TRIANGLES, 6, 3 );
+    gl.drawArrays( gl.TRIANGLES, 7, 3 );
+
+    gl.drawArrays( gl.TRIANGLES, 10, 3 );
+    gl.drawArrays( gl.TRIANGLES, 11, 3 );
+    gl.drawArrays( gl.TRIANGLES, 13, 3 );
+    gl.drawArrays( gl.TRIANGLES, 14, 3 );
+    gl.drawArrays( gl.TRIANGLES, 15, 3 );
+    gl.drawArrays( gl.TRIANGLES, 16, 3 );
+    gl.drawArrays( gl.TRIANGLES, 17, 3 );
+    gl.drawArrays( gl.LINES, 19, 2 );
+    gl.drawArrays( gl.TRIANGLES, 20, 3 );
+
+    gl.drawArrays( gl.TRIANGLES, 23, 3 );
+    gl.drawArrays( gl.TRIANGLES, 24, 3 );
+    gl.drawArrays( gl.TRIANGLES, 27, 3 );
+    gl.drawArrays( gl.TRIANGLES, 28, 3 );
+    gl.drawArrays( gl.TRIANGLES, 29, 3 );
+    gl.drawArrays( gl.TRIANGLES, 30, 3 );
+    gl.drawArrays( gl.TRIANGLES, 31, 3 );
+    gl.drawArrays( gl.LINES, 33, 2 );
+    gl.drawArrays( gl.TRIANGLES, 34, 3 );
+    gl.drawArrays( gl.TRIANGLES, 35, 3 );
+
+    if ( start ) {
+        if ( direction ) {
+            theta += 0.1;
+        } else {
+            theta -= 0.1;
+        }      
+        setTimeout(
+            function () { requestAnimationFrame(render); }, delay 
+        );
+    };
+}
